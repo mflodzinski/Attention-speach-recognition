1b282f9842fa805e2330aa3d75f9387c0cdedf9d
mflodzinski <mf.lodzinski@gmail.com>
2023-11-08 21:35:37 +0100

    asd

--- files/asd.py
+++
@@ -1,9 +0,0 @@
-import warprnnt_numba
-
-fastemit_lambda = 0.001  # any float >= 0.0
-
-loss_pt = warprnnt_numba.RNNTLossNumba(
-    blank=4, reduction="sum", fastemit_lambda=fastemit_lambda
-)
-
-print(len("she_had_your_dark_suit_in_greasy_wash_water_all_year"))
